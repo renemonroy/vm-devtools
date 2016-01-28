30154842ea5ce8e2eeaf1f2f54a1a1e40a1b367c
René Monroy <rene.monroy@gmail.com>
2016-01-27 19:24:00 -0600

    Add app icon to tray bar

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Tray = electron.Tray;
 
 var mainWindow = null;
+var appIcon = null;
 
 app.dock.hide();
 
@@ -13,6 +15,9 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+  appIcon = new Tray('./src/images/icon-tray-default@4x.png');
+  appIcon.setPressedImage('./src/images/icon-tray-highlight@4x.png');
+
   mainWindow = new BrowserWindow({
     width : 570,
     height : 750,
@@ -20,16 +25,24 @@ app.on('ready', function() {
     maxWidth : 900,
     minHeight : 700,
     maxHeight : 1000,
-    alwaysOnTop : true,
     frame : false
   });
+
   if ( process.env && process.env.NODE_ENV == 'development' ) {
     mainWindow.loadURL('http://localhost:8081');
   } else {
     mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
   }
-  mainWindow.webContents.openDevTools();
+
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
+  appIcon.on('click', function(e) {
+    if ( mainWindow.isFocused() ) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  }.bind(this));
 });
