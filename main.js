7e36bab713903138ff8e2d18e4c6d5bebee5a514
René Monroy <rene.monroy@gmail.com>
2016-01-29 22:43:23 -0600

    Open webkit devtools for now

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ app.on('ready', function() {
     mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
   }
 
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function() {
     mainWindow = null;
