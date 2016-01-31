aa1b80c3bd4e1790ef2eea1994d751329f8838d2
René Monroy <rene.monroy@gmail.com>
2016-01-30 23:33:14 -0600

    Add style state when window focus/blur events are triggered

--- src/components/ui/UITitlebar.jsx
+++ src/components/ui/UITitlebar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Radium from 'radium';
 
+const remote = window.require('remote');
+const currentWindow = remote.getCurrentWindow();
+
 /** UITitlebar Class
  *----------------------------------------------------------------------------*/
 @Radium
@@ -8,18 +11,34 @@ class UITitlebar extends React.Component {
 
   static displayName = 'UITitlebar';
 
+  constructor(props) {
+    super(props);
+    this.state = { windowState : 'normal' };
+  }
+
+  componentDidMount() {
+    currentWindow.on('blur', () => this.setState({
+      windowState : 'blur'
+    }));
+    currentWindow.on('focus', () => this.setState({
+      windowState : 'focus'
+    }));
+  }
+
   render() {
     const { leftItem, centerItem, rightItem } = this.props,
-      { titlebarStyle, leftItemStyle, rightItemStyle, centerItemStyle } = styles;
+      { windowState } = this.state,
+      { titlebarStyle, winStateStyle } = styles;
+    const titlebarStyles = [titlebarStyle, winStateStyle[windowState]];
     return (
-      <div id="titlebar" style={titlebarStyle}>
-        <div className="left-item" style={leftItemStyle}>
+      <div id="titlebar" style={titlebarStyles}>
+        <div className="left-item" style={styles.leftItemStyle}>
           {leftItem ? leftItem() : null}
         </div>
-        <div className="center-item" style={centerItemStyle}>
+        <div className="center-item" style={styles.centerItemStyle}>
           {centerItem ? centerItem() : null}
         </div>
-        <div className="right-item" style={rightItemStyle}>
+        <div className="right-item" style={styles.rightItemStyle}>
           {rightItem ? rightItem() : null}
         </div>
       </div>
@@ -33,9 +52,6 @@ class UITitlebar extends React.Component {
 const styles = {
   titlebarStyle : {
     position : 'absolute',
-    backgroundColor : 'rgb(251, 251, 251)',
-    backgroundImage : 'linear-gradient(#f4f4f4, #d4d1d4)',
-    borderBottom : '1px solid rgba(0, 0, 0, .25)',
     zIndex : 3,
     top : 0,
     left : 0,
@@ -49,7 +65,9 @@ const styles = {
     flexDirection : 'row',
     WebkitAlignItems : 'center',
     alignItems : 'center',
-    WebkitAppRegion : 'drag'
+    WebkitAppRegion : 'drag',
+    fontFamily : '"Helvetica Neue", Helvetica, sans-serif',
+    fontSize : '1.2rem'
   },
   leftItemStyle : {
     width : '5rem',
@@ -77,14 +95,26 @@ const styles = {
   },
   centerItemStyle : {
     height : '100%',
-    WebkitTransition : 'all 0.3s',
-    transition : 'all 0.3s',
     position : 'relative',
     display : 'flex',
     WebkitFlexDirection : 'row',
     flexDirection : 'row',
     WebkitAlignItems : 'center',
     alignItems : 'center'
+  },
+  winStateStyle : {
+    blur : {
+      borderBottom : '1px solid rgba(0, 0, 0, .1)',
+      color : '#acacac',
+      backgroundColor : '#f6f6f6',
+      backgroundImage : 'none'
+    },
+    focus : {
+      borderBottom : '1px solid rgba(0, 0, 0, .25)',
+      color : '#484848',
+      backgroundColor : 'rgb(251, 251, 251)',
+      backgroundImage : 'linear-gradient(#f4f4f4, #d4d1d4)'
+    }
   }
 };
 
