a405e62d062e054f4fc6a320cda5bdcfc12673ff
René Monroy <rene.monroy@gmail.com>
2016-01-30 23:31:32 -0600

    Update main app index with custom components for Sidebar

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Radium from 'radium';
-import { UIContent, UISidebar, UITitlebar, UIScenesGroup, UIWinControls, UIIcon } from './ui';
+import { UIContent, UISidebar, UITitlebar, UIScenesGroup, UIWinControls } from './ui';
+import { Navigation } from './blocks';
 import { SidebarToggler } from './buttons';
+
+require('../images/vm-logo.png');
 
 /** App Class
  *----------------------------------------------------------------------------*/
@@ -12,12 +15,13 @@ class App extends React.Component {
 
   render() {
     const { children, location:loc } = this.props,
-      { appStyle, titleStyle } = styles;
+      { appStyle, titleStyle, logoStyle } = styles;
     return (
       <div id="app" style={styles.appStyle}>
         <UISidebar>
-          <UIWinControls />
-          <UIIcon name="rocket" color="#fff" />
+          <UIWinControls/>
+          <img src="/images/vm-logo.png" style={logoStyle}/>
+          <Navigation/>
         </UISidebar>
         <UIContent>
           <UITitlebar
@@ -26,23 +30,27 @@ class App extends React.Component {
             scenes={() => children}
             sceneRoute={loc.pathname}/>
         </UIContent>
-       </div>
-     );
-   }
+      </div>
+    );
+  }
 
- };
+};
 
- /** App Styles
-  *----------------------------------------------------------------------------*/
- const styles = {
-   appStyle : {
-     backgroundColor : '#fff'
-   },
-   titleStyle : {
-     color : '#484848',
-     fontFamily : '"Helvetica Neue", Helvetica, sans-serif',
-     fontSize : '1.3rem'
-   }
- };
+/** App Styles
+ *----------------------------------------------------------------------------*/
+const styles = {
+  appStyle : {
+    backgroundColor : '#fff'
+  },
+  titleStyle : {
+    marginTop : '1px'
+  },
+  logoStyle : {
+    maxWidth : '3.7rem',
+    height : 'auto',
+    display : 'block',
+    margin : '1.3rem auto'
+  }
+};
 
- export default App;
+export default App;
