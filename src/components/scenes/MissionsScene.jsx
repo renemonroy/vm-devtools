3234e92a9e1698c4f292380f6a9e80ec8495414f
René Monroy <rene.monroy@gmail.com>
2016-02-03 16:40:20 -0600

    Render data from a specific mission to do tests

--- src/components/scenes/MissionsScene.jsx
+++ src/components/scenes/MissionsScene.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { UIScene, UIInputTag } from '../ui';
+import * as missionActions from '../../actions/Mission';
 
 /** MissionScene Class
  *----------------------------------------------------------------------------*/
+@connect(state => ({
+  missions : state.MissionStore.get('missions').toJS()
+}))
 class MissionsScene extends React.Component {
 
   static displayName = 'MissionsScene';
 
+  static propTypes = {
+    missions : React.PropTypes.object
+  };
+
+  handleScreensChange(tags) {
+    this.props.dispatch(missionActions.editMission('com.virginmegausa.mission.view-media', { screens : tags }));
+  }
+
   render() {
+    const { missions } = this.props;
+    const mission = missions['com.virginmegausa.mission.view-media'];
     return (
       <UIScene name="missions">
         <h3>Missions names</h3>
         <UIInputTag
+          tags={mission.screens}
           placeholder="Add a screen name"
-          onChange={(tags) => {console.log('tags:', tags)}}/>
+          onChange={::this.handleScreensChange}/>
       </UIScene>
     );
   }
