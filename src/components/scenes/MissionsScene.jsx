afb556f410e4bbfba6f9d0a7f086b8515d6a077a
René Monroy <rene.monroy@gmail.com>
2016-02-06 21:28:15 -0600

    Components will dispatch actions and should not bind external events from now on

--- src/components/scenes/MissionsScene.jsx
+++ src/components/scenes/MissionsScene.jsx
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { UIScene, UIInputTag } from '../ui';
 import * as missionActions from '../../actions/Mission';
 
-var ipcRenderer = require('electron').ipcRenderer;
-
 /** MissionScene Class
  *----------------------------------------------------------------------------*/
 @connect(state => (state.Mission.toJS()))
@@ -13,43 +11,36 @@ class MissionsScene extends React.Component {
   static displayName = 'MissionsScene';
 
   static propTypes = {
-    sceneMode: PropTypes.oneOf([-1, 0, 1, 2, 3]),
-    missionsNames: PropTypes.array,
-    activeMission: PropTypes.object
+    missionsList: PropTypes.shape({
+      status: PropTypes.oneOf([-1, 0, 1, 2, 3]),
+      data: PropTypes.array.isRequired
+    }),
+    activeMission: PropTypes.shape({
+      status: PropTypes.oneOf([-1, 0, 1, 2, 3]),
+      data: PropTypes.object.isRequired
+    }),
   };
 
-  componentWillMount() {
-    ipcRenderer.on('missions:res:list', ::this.onMissionsNames);
-  }
-
   componentDidMount() {
-    ipcRenderer.send('missions:req:list');
-  }
-
-  componentWillUnmount() {
-    ipcRenderer.removeListener('missions:res:list', ::this.onMissionsNames);
-  }
-
-  onMissionsNames(e, names) {
-    this.props.dispatch(missionActions.updateMissionsNames(names));
+    this.props.dispatch(missionActions.getMissionsList());
   }
 
   handleScreensChange(tags) {
-    this.props.dispatch(missionActions.editActiveMission({ screens : tags }));
+    this.props.dispatch(missionActions.updateActiveMission({ screens: tags }));
   }
 
   render() {
-    const { missionsNames, activeMission } = this.props;
+    const { missionsList, activeMission } = this.props;
     return (
       <UIScene name="missions">
         <ul>
-          {missionsNames.map((mn) =>
-            <li>{mn}</li>
+          {missionsList.data.map((mn, i) =>
+            <li key={'mission-' + i}>{mn}</li>
           )}
         </ul>
         <h3>Scenes names</h3>
         <UIInputTag
-          tags={activeMission.screens}
+          tags={activeMission.data.screens}
           placeholder="Add a screen name"
           onChange={::this.handleScreensChange}/>
       </UIScene>
