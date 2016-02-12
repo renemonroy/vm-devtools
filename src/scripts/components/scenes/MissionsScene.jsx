275155eb447e685916d4cccc69302761c0e53141
René Monroy <rene.monroy@gmail.com>
2016-02-12 01:15:15 -0600

    Use scene header component and render it into scene as part of the layout

--- src/scripts/components/scenes/MissionsScene.jsx
+++ src/scripts/components/scenes/MissionsScene.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { UIScene } from '../ui';
-import { MissionsList, ActiveMission } from '../blocks';
+import { SceneHeader, MissionsList, ActiveMission } from '../blocks';
 import { MissionActions } from '../../actions';
 
 /** MissionScene Class
@@ -20,6 +20,15 @@ class MissionsScene extends React.Component {
     this.props.dispatch(MissionActions.loadMissionsList());
   }
 
+  renderHeader() {
+    return (
+      <SceneHeader
+        heading="Mission Boilerplate"
+        description="Set the values to generate the boostrap code of your next mission. Remember, is not a builder but only a starter. Code and configuration are saved in File System, check missions folder.</p>"
+      />
+    );
+  }
+
   renderSidebar() {
     const { missionsList:ml } = this.props;
     return <MissionsList status={ml.status} data={ml.data} />;
@@ -34,6 +43,7 @@ class MissionsScene extends React.Component {
     const { missionsList, activeMission } = this.props;
     return (
       <UIScene
+        header={::this.renderHeader}
         sidebar={::this.renderSidebar}
         content={::this.renderContent}
         name="missions"/>
