46a54b14fa13a065a748ebfdeb31e65ae4b5ea84
René Monroy <rene.monroy@gmail.com>
2016-02-24 01:02:41 -0600

    UIForm only renders forms for now but it will handle the generator eventually

---
+++ src/scripts/components/ui/UIForm.jsx
@@ -0,0 +1,31 @@
+import React, { PropTypes } from 'react';
+
+/** UIForm Class
+ *----------------------------------------------------------------------------*/
+class UIForm extends React.Component {
+
+  static displayName = 'UIForm';
+
+  static propTypes = {
+    children: PropTypes.any.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    debugger;
+    this.props.onSubmit(e);
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <form onSubmit={::this.handleSubmit}>
+        {children}
+      </form>
+    );
+  }
+
+}
+
+export default UIForm;
